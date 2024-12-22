a6dc33203846f8a552c0a7db9fdec90c3ef8b651
ashfaquertc <ashfaquehossain500@outlook.com>
2024-12-22 16:26:33 +0600

    Updated

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-// Nav Menu Functionality
+// ==========================================================
+// -- Nav Menu Functionality
+// ==========================================================
+
 const nav = document.querySelector("nav");
 const navIcon = document.querySelector(".mobile-nav-icon");
 const navItems = document.querySelector(".mobile-menu-items");
@@ -17,7 +20,11 @@ window.onscroll = function() {
   }
 }
 
-// Menu Icon Functionality
+
+// ==========================================================
+// -- Menu Icon Functionality
+// ==========================================================
+
 function iconFunc(icon) {
   icon.classList.toggle("change");
 }
@@ -32,10 +39,49 @@ menuIcon.addEventListener("click", () => {
     barIcon.classList.toggle("fa-x");
 });
 
-// Banner Slider
+
+// ==========================================================
+// -- Banner Slider
+// ==========================================================
+
 $('.banner-slider').slick({
     arrows: false,
     dots: true,
     autoplay: true,
     // fade: true,
+});
+
+
+// ==========================================================
+// -- Review Slider
+// ==========================================================
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
 });
